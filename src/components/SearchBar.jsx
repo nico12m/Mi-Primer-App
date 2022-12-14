f1df5cbefaaaa02e60a55fced2fd5d2fb1065d03
nico12m <mojavik.nm@gmail.com>
2022-12-13 21:41:43 -0300

    fun aleatoria acomodada

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,11 +6,17 @@ export default function SearchBar(props) {
    const handleChange = (valor)=>{
       setId(valor.target.value)
    }
+   const numAzar = (id)=>{
+     let min = Math.ceil(1);
+     let max = Math.floor(826);
+      let num = Math.floor(Math.random() * (max - min) + min);
+      if(num!==id) {return num} else {numAzar(id)}
+   }
    return (
       <div>
       <input type='search'name="search" id="" onChange={handleChange} placeholder="Buscar Por ID" />
       <button onClick={()=>props.onSearch(id)}>Agregar</button>
-      <button onClick={()=>props.onSearch(Math.ceil(Math.random()*100))}>Aleatorio</button>
+      <button onClick={()=>props.onSearch(numAzar(id))}>Aleatorio</button>
       </div>
    );
 }
